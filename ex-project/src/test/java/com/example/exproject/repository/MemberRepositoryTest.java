7072f47db0519fc777464f3cb6629c0aeb3b0ee2
calmmin <mj3242@naver.com>
2023-06-20 22:27:18 +0900

    feature : MemberRepository 테스트 클래스 추가(#2) - MemberRepository 테스트 클래스 설정 - 유저가 입력한 아이디, 패스워드를 해시값으로 변환하는 hashSHA256 메서드 추가 - save(), findById() 를 테스트하는 testMember 메서드 추가

---
+++ ex-project/src/test/java/com/example/exproject/repository/MemberRepositoryTest.java
@@ -0,0 +1,69 @@
+package com.example.exproject.repository;
+
+import com.example.exproject.domain.Member;
+import com.example.exproject.domain.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public String hashSHA256(String input) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] encodedHash = digest.digest(input.getBytes(StandardCharsets.UTF_8));
+
+            //Convert the byte to a hexadecimal string
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : encodedHash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Test
+    public void testMember() {
+        String memberCustomId = "exampleId";
+        String password = "examplePw";
+        
+        /**
+         * Spring Security 같은 경우를 활용하면 사용자의 ID, PW 등을
+         * 해시함수를 활용해 변환하여 DB에 저장하는 경우가 많다.
+         */
+        String hashedMemberCustomId = hashSHA256(memberCustomId);
+        String hashedPw = hashSHA256(password);
+        Member member = new Member(hashedMemberCustomId, hashedPw, "exName");
+
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+}
