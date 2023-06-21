acaf9ea7a211dd576b33ab69fcafb6eca1fc9658
calmmin <mj3242@naver.com>
2023-06-21 13:58:39 +0900

    feature : @Rollback(false) 어노테이션 삭제

--- ex-project/src/test/java/com/example/exproject/repository/MemberRepositoryTest.java
+++ ex-project/src/test/java/com/example/exproject/repository/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.example.exproject.repository;
 
 import com.example.exproject.domain.Member;
-import com.example.exproject.domain.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +9,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
